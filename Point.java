94fe2eabfcd67b44cf34c705a957fede29d7eb6b
Rupa <110527772+SIRAPURUPA@users.noreply.github.com>
2022-10-25 18:32:21 +0530

    Update Point.java

--- Point.java
+++ Point.java
@@ -8,36 +8,13 @@
  *
  ******************************************************************************/
 
-import edu.princeton.cs.algs4.StdDraw;
-
 import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class Point implements Comparable<Point> {
 
     private final int x;     // x-coordinate of this point
     private final int y;     // y-coordinate of this point
-
-    // Points comparator
-    private class PointComparator implements Comparator<Point>
-    {
-        // compare two points
-        public int compare(Point p1, Point p2)
-        {
-            double p1Slope = slopeTo(p1);
-            double p2Slope = slopeTo(p2);
-
-            if (p1Slope > p2Slope) {
-                return 1;
-            }
-
-            if (p1Slope < p2Slope) {
-                return -1;
-            }
-
-            // if equals
-            return 0;
-        }
-    }
 
     /**
      * Initializes a new point.
@@ -82,25 +59,14 @@ public class Point implements Comparable<Point> {
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
-        double ySlope = that.y - y;
-        double xSlope = that.x - x;
+        if (this.x == that.x) 
+            if (this.y == that.y) return Double.NEGATIVE_INFINITY;  // Both points are the same
+            else return Double.POSITIVE_INFINITY;  // Vertical line
 
-        // (x0, y0) and (x1, y1) are equal
-        if (ySlope == 0 && xSlope == 0) {
-            return Double.NEGATIVE_INFINITY;
-        }
+        if (this.y == that.y) 
+            return 0.0;  // Horizontal line
 
-        // line segment is horizontal
-        if (ySlope == 0) {
-            return +0.0;
-        }
-
-        // line segment is vertical
-        if (xSlope == 0) {
-            return Double.POSITIVE_INFINITY;
-        }
-
-        return (ySlope / xSlope);
+        else return ((double) (that.y - this.y) / (double) (that.x - this.x));
     }
 
     /**
@@ -116,26 +82,12 @@ public class Point implements Comparable<Point> {
      *         argument point
      */
     public int compareTo(Point that) {
-        // first check by 'y'
-        if (y > that.y) {
-            return 1;
+        int difY = this.y - that.y;
+        if (difY != 0) {
+            return difY;
+        } else {
+            return (this.x - that.x);
         }
-
-        if (y < that.y) {
-            return -1;
-        }
-
-        // then check by 'x'
-        if (x > that.x) {
-            return 1;
-        }
-
-        if (x < that.x) {
-            return -1;
-        }
-
-        // if equals
-        return 0;
     }
 
     /**
@@ -145,9 +97,25 @@ public class Point implements Comparable<Point> {
      * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        return new PointComparator();
+        return new BySlope(this);
     }
 
+    private static class BySlope implements Comparator<Point> {
+        private final Point p;
+
+        public BySlope(Point p) { 
+            this.p = p;
+        }
+
+        public int compare(Point p1, Point p2) {
+            double slopeTo1 = this.p.slopeTo(p1);
+            double slopeTo2 = this.p.slopeTo(p2);
+
+            if (slopeTo1 > slopeTo2) return 1;
+            if (slopeTo1 < slopeTo2) return -1;
+            else return 0;
+        }
+    }
 
     /**
      * Returns a string representation of this point.
@@ -165,41 +133,31 @@ public class Point implements Comparable<Point> {
      * Unit tests the Point data type.
      */
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
-        Point p1 = new Point(10, 20);
-        Point p2 = new Point(20, 25);
-        Point p3 = new Point(10, 20);
-
-        Point p4 = new Point(10, 20);
-        Point p5 = new Point(1, 20);
-        Point p6 = new Point(5, 1);
-        Point p7 = new Point(32128, 32128);
-        Point p8 = new Point(32128, 32128);
-        Point p9 = new Point(10, 123);
-
-        Point p10 = new Point(10, 30);
-        Point p11 = new Point(30, 20);
-
-        assert (p1.compareTo(p2) < 0) : p1 + " " + p2;
-        assert (p1.compareTo(p3) == 0) : p1 + " " + p3;
-        assert (p2.compareTo(p3) > 0) : p2 + " " + p3;
-
-        assert (p4.compareTo(p5) > 0) : p4 + " " + p5;
-        assert (p5.compareTo(p4) < 0) : p5 + " " + p4;
-        assert (p5.compareTo(p6) > 0) : p5 + " " + p6;
-        assert (p4.compareTo(p6) > 0) : p4 + " " + p6;
-        assert (p7.compareTo(p8) == 0) : p7 + " " + p8;
+        Point p0 = new Point(0, 0);
 
 
-        assert (Double.compare(p7.slopeTo(p8), Double.NEGATIVE_INFINITY) == 0) : "Wrong value for degraded line";
-        assert (Double.compare(p4.slopeTo(p9), Double.POSITIVE_INFINITY) == 0) : "Wrong value for vertical line";
-        assert (Double.compare(p4.slopeTo(p5), +0.0) == 0) : "Wrong value for horizontal line";
-        assert (Double.compare(p1.slopeTo(p2), 0.5) == 0);
-        assert (Double.compare(p10.slopeTo(p11), -0.5) == 0);
+        System.out.println("Testing slopeTo");
+        System.out.println(p0.slopeTo(new Point(0, 0))); // Double.NEGATIVE_INFINITY;
+        System.out.println(p0.slopeTo(new Point(0, 1))); // Double.POSITIVE_INFINITY;
+        System.out.println(p0.slopeTo(new Point(3, 0))); // 0.0
+        System.out.println(p0.slopeTo(new Point(1, 1))); // 1.0
+        System.out.println(p0.slopeTo(new Point(-232, -232))); // 1.0
+        System.out.println(p0.slopeTo(new Point(-1, 1))); // -1.0
+        System.out.println(p0.slopeTo(new Point(2, 1))); // 0.5
+        System.out.println(p0.slopeTo(new Point(1, 2))); // 2.0
+        System.out.println(p0.slopeTo(new Point(1, 30))); // 30.0
+        System.out.println(p0.slopeTo(new Point(30, 1))); // aprox 0.033333
 
-
-        assert (p4.slopeOrder().compare(p3, p5) < 0);
-        assert (p4.slopeOrder().compare(p5, p3) > 0);
-        assert (p1.slopeOrder().compare(p7, p8) == 0);
+        System.out.println("\n");
+        System.out.println("Testing compareTo");
+        System.out.println(p0.compareTo(new Point(0, 0))); // 0
+        System.out.println(p0.compareTo(new Point(1, 0))); // 1
+        System.out.println(p0.compareTo(new Point(2, -1))); // -1
+        System.out.println(p0.compareTo(new Point(-1, 0))); // -1
+        System.out.println(p0.compareTo(new Point(0, -0))); // 0
+        System.out.println(p0.compareTo(new Point(2, 2))); // 1
+        System.out.println(p0.compareTo(new Point(-2, 5))); // 1
+        System.out.println(p0.compareTo(new Point(333, -0))); // 1
+        System.out.println(p0.compareTo(new Point(0, 3333))); // 1
     }
 }
